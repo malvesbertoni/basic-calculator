7c4c99acc6d6bdd509d0671b38a9162d9c884196
malvesbertoni <malvesbertoni@gmail.com>
2021-02-02 16:57:44 -0300

    Finished javascript

--- script.js
+++ script.js
@@ -1,52 +1,95 @@
-window.onload = function() {
-    const buttonsContainer = document.querySelector('.buttons-container');
-    buttonsContainer.addEventListener('click', function(event) {
-        
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector(".result-container");
 
-
-        if (event.target.tagName === 'BUTTON') {
-            //alert(`You clicked on button ${event.target.innerText}`);
-
-            printResult(event.target.innerText);
-        }
-    
-
-
-      });
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
 }
 
-
-function printResult(value) {
-    let printedResult = document.querySelector('.printed-result');
-    
-    if (value === 'C')
-        printedResult.innerText = '0';
-
-    else if (value === '←')
-        printedResult.innerText = printedResult.innerText.slice(0,-1);
-    
-    else if (value === '+') {
-        calculateSum(printedResult.innerText);
-        
-        
-    }
-
-    else if (value === '÷' || value === '×' || value === '−'
-        || value === '+' || value === '=') {
-        alert("You have clicked on a special button");
-        printedResult.innerText = '0'
-    }
-
-    else if (printedResult.innerText === '0')
-        printedResult.innerText = value;
-
-    else if (printedResult.innerText !== '0')
-    printedResult.innerText = printedResult.innerText.concat(value);
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
 }
 
-function calculateSum(value) {
-    value = parseInt(value,10);
-   
+function handleMath(value) {
+  if (buffer === "0") {
+    // do nothing
+    return;
+  }
 
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
 
+  previousOperator = value;
+
+  buffer = "0";
 }
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = +runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      handleMath(value);
+      break;
+  }
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
+
+function init() {
+  document.querySelector(".buttons-container").addEventListener("click", function(event) {
+    buttonClick(event.target.innerText);
+  });
+}
+
+init();
